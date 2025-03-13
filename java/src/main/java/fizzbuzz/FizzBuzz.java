eb67fd289a32324859c9fc7f682594f02a0a96ff
Lolean <loloris2009@gmail.com>
2025-03-13 23:18:51 +0100

    fizzbuzz and t9 test completed

--- java/src/main/java/fizzbuzz/FizzBuzz.java
+++ java/src/main/java/fizzbuzz/FizzBuzz.java
@@ -1,9 +1,29 @@
 package fizzbuzz;
 
-public class FizzBuzz {
+public class FizzBuzz
+{
 
-    public String pickOne(int input) {
-        return "fizz or buzz";
+    public String pickOne(int input)
+	{
+        
+        String result = Integer.toString(input);
+
+        if( input % 3 == 0 )
+		{
+            result = "fizz";
+        }
+
+        if( input % 5 == 0) 
+		{
+			if(result == "fizz")
+				return "fizzbuzz";
+			
+			result = "buzz";
+        }
+
+        return result;
+
+
     }
 
 }
--- java/src/main/java/t9/T9Translator.java
+++ java/src/main/java/t9/T9Translator.java
@@ -1,9 +1,11 @@
 package t9;
 
+
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Arrays;
-import java.util.stream.Collectors;
+import java.util.Scanner;
+
+
 
 public class T9Translator {
 
@@ -24,8 +26,33 @@ public class T9Translator {
      * For instance,  to write "hihi": "44 444 44 444"
      * Pauses can be repeated multiple time, including between two different key presses and should not impact output
      */
-    public String translate(InputStream stream) throws IOException {
-        return "";
-    }
+    
 
+    public String translate(InputStream stream) throws IOException {
+
+        // ASCII values of every first letters of the keyboard
+        int[] ASCII = {32,0,97,100,103,106,109,112,116,119};
+
+        String result = "";
+
+        // Input stream translation into String
+        Scanner s = new Scanner(stream).useDelimiter("\\A");
+        String toDecode = s.hasNext() ? s.next() : "";
+        toDecode+='\0';
+        s.close();
+        
+        for(int i=0;i<toDecode.length();i++)
+        {
+            while(toDecode.charAt(i)==32) i++;
+            int count = 0;
+            int index = toDecode.charAt(i)-48;
+            while(toDecode.charAt(i) != '\0' && toDecode.charAt(i+count+1)-48 == index) count ++; 
+            if(index >= 0)
+            {
+                result += Character.toString(ASCII[index]+count);
+                i+=count;
+            }
+        }
+        return result;
+    }
 }
